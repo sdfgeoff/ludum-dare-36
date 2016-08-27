ceca51300c70aa48424d3ee98bb9e648d8ab70fa
Lambosaurus <tlamborn@gmail.com>
2016-08-27 18:47:08 +1200

    Put player script in separate folder

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,29 +1,35 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-const GRAVITY = 200.0
-const WALK_SPEED = 200
+const GRAVITY = 10.0
+const WALK_SPEED = 20
 
 var velocity = Vector2()
 
 
+
 func _fixed_process(delta):
 
-    velocity.y += delta * GRAVITY
-    if (Input.is_action_pressed("ui_left")):
+    velocity.y = GRAVITY
+    if (Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_A) ):
         velocity.x = - WALK_SPEED
-    elif (Input.is_action_pressed("ui_right")):
+    elif (Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D)):
         velocity.x =   WALK_SPEED
     else:
         velocity.x = 0
 
     var motion = velocity * delta
-    motion = move(motion)
+    #motion = move(motion)
+    
+    set_rot(0.0)
 
-    if (is_colliding()):
-        var n = get_collision_normal()
-        motion = n.slide(motion)
-        velocity = n.slide(velocity)
-        move(motion)
+    apply_impulse( Vector2(0,0), velocity )
+
+    #more comments
+    #if (is_colliding()):
+    #    var n = get_collision_normal()
+    #    motion = n.slide(motion)
+    #    velocity = n.slide(velocity)
+    #    move(motion)
 
 
 func _ready():
