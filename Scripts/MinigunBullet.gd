65f3845ee34984ad8ba6cbcd1c4123fc5062baca
sdfgeoff <sdfgeoff@gmail.com>
2016-08-27 21:21:47 +1200

    Added bullets

---
+++ Scripts/MinigunBullet.gd
@@ -0,0 +1,16 @@
+
+extends RigidBody2D
+
+const SPEED = 100.0
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_max_contacts_reported(5)
+	set_contact_monitor(true)
+	set_linear_velocity(Vector2(SPEED,0.0))
+
+
+
+func _on_MinigunBullet_body_enter( body ):
+	queue_free()
---
+++ Scripts/minigun.gd
@@ -0,0 +1,18 @@
+
+const RELOAD_TIME = 0.5 # seconds
+
+var bullet = preload("res://Assets/Projectiles/MinigunBullet.tscn")
+var cool_down = 0
+
+func _fixed_process(delta): 
+	cool_down += delta
+	fire_gun()
+	
+func fire_gun():
+	if cool_down > RELOAD_TIME:
+		add_child(bullet.instance())
+		cool_down = 0
+
+func _ready():
+	add_child(bullet.instance())
+	set_fixed_process(true)
